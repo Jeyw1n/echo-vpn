00c948ecf459413535958088b14c09f73eab0555
Jeywin <j3ywin@gmail.com>
2024-09-26 22:24:23 +0300

    Ссылка-приглашение, сохранение ID инвайтора в бд, вывод кол-ва ключей

--- aiogram_bot/routers/menu.py
+++ aiogram_bot/routers/menu.py
@@ -19,7 +19,6 @@ import database
 from datetime import datetime, timedelta
 import sys
 
-import utils
 import database
 
 from aiogram_bot.markups import menu_keyboard
@@ -33,34 +32,48 @@ photo_4 = FSInputFile('media/guide_4.png')
 photo_support = FSInputFile('media/support.png')
 
 
-@menu_router.message(F.text == "📖 Инструкция")
-async def instruction(msg: Message):
+@menu_router.message(F.text == '📖 Инструкция')
+async def instruction(msg: Message) -> None:
     await msg.answer(texts['guide_1'], parse_mode='Markdown', disable_web_page_preview=True)
     await msg.answer_photo(photo=photo_2, caption=texts['guide_2'])
     await msg.answer_photo(photo=photo_3, caption=texts['guide_3'])
     await msg.answer_photo(photo=photo_4, caption=texts['guide_4'])
 
 
-@menu_router.message(F.text == "🔑 Ключи")
-async def show_keys(msg: Message):
+@menu_router.message(F.text == '🔑 Ключи')
+async def show_keys(msg: Message) -> None:
     keys = database.get_user_keys(msg.from_user.id)
     if not keys:
         # Добавить кнопку "💳 Оплата"
-        await msg.answer("У вас нет ключей. Необходимо произвести оплату.")
+        await msg.answer('У вас нет ключей. Необходимо произвести оплату.')
+        return
+
+    keys_count = len(keys)
+    keys_word = 'ключ' if keys_count == 1 else 'ключа' if keys_count < 5 else 'ключей'
+    # Если все-таки ключи есть
+    await msg.answer(f'У вас {len(keys)} {keys_word}. 🔑\nВы можете купить еще, просто нажав *\"💳 Новый ключ\"*\n\n'
+                     f'*Текущий список ваших ключей:*')
     for key in keys:
         remaining_time = database.get_remaining_time(key.key_id)
-        formatted_time = utils.format_remaining_time(remaining_time)
-        await msg.answer(f"*Сервер:* VendekVPN ► Netherlands 🇳🇱\n\n"
-                         f"Ваш ключ: _#{key.key_id}_\n"
-                         f"Истекает через: _{formatted_time}_"
-                         f"```{key.access_url + texts['connection_name']}```")
+        # formatted_time = utils.format_remaining_time(remaining_time)
+        await msg.answer(f'*Сервер:* VendekVPN ► Netherlands 🇳🇱\n\n'
+                         f'Ваш ключ: _#{key.key_id}_\n'
+                         f'Истекает через: _{remaining_time}_'
+                         f'```{key.access_url + texts['connection_name']}```')
+    return
 
 
-@menu_router.message(F.text == "💬 Поддержка")
-async def support(msg: Message):
+@menu_router.message(F.text == '💬 Поддержка')
+async def support(msg: Message) -> None:
     await msg.answer_photo(photo=photo_support, caption=texts['support_message'])
 
 
-@menu_router.message(F.text == "ℹ️ Инфо")
-async def info(msg: Message):
+@menu_router.message(F.text == 'ℹ️ Инфо')
+async def info(msg: Message) -> None:
     await msg.answer(texts['about'])
+
+
+@menu_router.message(F.text == 'Рефералы 🤝')
+async def info(msg: Message) -> None:
+    await msg.answer(f'Пока-что в разработке, но ваши приглашения засчитываются.\n'
+                     f'Ваша ссылка-приглашение:\nhttps://t.me/vendek\_vpn\_bot/?start={msg.from_user.id}')
--- aiogram_bot/routers/start.py
+++ aiogram_bot/routers/start.py
@@ -24,13 +24,22 @@ start_router = Router()
 
 @start_router.message(Command('start'))
 async def start_command(msg: Message) -> None:
-    user_id: int = msg.from_user.id
-    # Сохраняем пользователя в базу данных, если его еще там нет.
-    # Сохраняем это в db_save_result (True - сохранился, False - уже есть в БД)
-    db_save_result: bool = database.add_user(user_id)
 
-    if db_save_result:
-        # Если пользователь новый, то даём 10ти дневный доступ.
+    user_id: int = msg.from_user.id
+    user_exists: bool = database.user_exists(user_id)
+
+    # Сработает, если пользователь является новым
+    if not user_exists:
+        # Ищем referrer ID и сохраняем его в БД к новому пользователю, если referrer ID существует
+        referrer_id = msg.text[7:]
+        if referrer_id != '' and referrer_id != str(msg.from_user.id):
+            database.add_user(user_id, referrer_id)
+            await msg.bot.send_message(chat_id=referrer_id, text=f'Пользователь @{msg.from_user.username} перешел по вашей ссылке! 🎉')
+        else:
+            database.add_user(user_id)
+
+        # Пользователь новый, поэтому даём 10ти дневный доступ
+        # и создадим новый ключ.
         expiration_date = datetime.now() + timedelta(days=10)
         database.add_key(user_id, expiration_date)
 
@@ -38,5 +47,6 @@ async def start_command(msg: Message) -> None:
         await msg.answer(texts['menu'], reply_markup=menu_keyboard())
 
     else:
-        await msg.answer(f'*Привет, {msg.from_user.first_name}!*\nЧем могу быть полезен сегодня?', reply_markup=menu_keyboard())
+        await msg.answer(f'*Привет, {msg.from_user.first_name}!*\nЧем могу быть полезен сегодня?',
+                         reply_markup=menu_keyboard())
         await msg.answer(texts['menu'], reply_markup=menu_keyboard())
