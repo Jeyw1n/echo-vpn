7fbb6f192ac833844d20a239861cf8e59a5c318f
Jeywin <j3ywin@gmail.com>
2024-09-30 22:49:14 +0300

    Логирование и проверка IP перед обработкой запроса

--- webhook.py
+++ webhook.py
@@ -7,26 +7,31 @@ from aiogram import Bot
 import config as conf
 import database
 
+# Настройка логирования
 logger.add("./logs/webhook.log")
 
 app = Quart(__name__)
 
+
 def is_trusted_ip(ip):
-    ip_obj = ipaddress.ip_address(ip)
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+    except ValueError:
+        logger.warning(f"Некорректный IP-адрес: {ip}")
+        return False
     for trusted_ip in conf.TRUSTED_IP:
         if ip_obj in ipaddress.ip_network(trusted_ip):
             return True
+    logger.warning(f"IP-адрес {ip} не является доверенным.")
     return False
+
 
 API_TOKEN = conf.BOT_TOKEN
 bot = Bot(token=API_TOKEN)
 
+
 async def handle_successful_payment(payment_id: str):
     """Обрабатывает успешный платеж."""
-    # Фильтруем только IP адреса от ЮМани
-    if not is_trusted_ip(request.remote_addr):
-        return abort(403)
-
     transaction = database.get_transaction(payment_id)
     logger.debug(transaction)
     if not transaction:
@@ -34,7 +39,7 @@ async def handle_successful_payment(payment_id: str):
         return jsonify({"status": "transaction not found"}), 404
 
     telegram_id = transaction.telegram_id
-    message_id = transaction.message_id
+    # message_id = transaction.message_id
     key_id = transaction.key_id
     months = transaction.months
 
@@ -43,7 +48,7 @@ async def handle_successful_payment(payment_id: str):
         logger.info(f"Транзакция {payment_id} обновлена на успешный статус.")
 
         # Удаляем сообщение с указанным message_id
-        await delete_message(telegram_id, message_id)
+        # await delete_message(telegram_id, message_id)
 
         # Продлеваем ключ или создаем новый
         if key_id == 'new':
@@ -61,16 +66,30 @@ async def handle_successful_payment(payment_id: str):
     return jsonify({"status": "success"}), 200
 
 
-async def delete_message(telegram_id: str, message_id: int):
-    """Удаляет сообщение в Telegram."""
-    try:
-        await bot.delete_message(chat_id=telegram_id, message_id=message_id)
-    except Exception as e:
-        logger.error(f"Ошибка при удалении сообщения: {e}")
+# async def delete_message(telegram_id: str, message_id: int):
+#     """Удаляет сообщение в Telegram."""
+#     try:
+#         await bot.delete_message(chat_id=telegram_id, message_id=message_id)
+#     except Exception as e:
+#         logger.error(f"Ошибка при удалении сообщения: {e}")
+
+@app.before_request
+async def before_request():
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
+    logger.debug(f"IP-адрес запроса: {ip}")
+
+    if not is_trusted_ip(ip):
+        logger.warning(f"Запрос от недоверенного IP-адреса: {ip}")
+        return abort(403)
 
 
 @app.route('/webhook', methods=['POST'])
 async def notification_webhook():
+    # # Фильтруем только IP адреса от ЮМани
+    # ip = request.remote_addr
+    # if not is_trusted_ip(ip):
+    #     return abort(403)
+
     data = await request.get_json()
     logger.debug(f"Webhook получил данные: {data}")
 
